f64c862d269ee78d61d118cae142efd572e57e10
unknown <aadislearningacnt@gmail.com>
2024-09-22 17:34:31 +0530

    Initial Project Setup With Spring MVC Web, PGSql, Validation etc dependencies

---
+++ src/main/java/com/aadi/DHHHeads/DhhHeadsApplication.java
@@ -0,0 +1,13 @@
+package com.aadi.DHHHeads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhhHeadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhhHeadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aadi/DHHHeads/DhhHeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadi.DHHHeads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhhHeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
